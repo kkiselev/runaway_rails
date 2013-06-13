acf16c4d2292557d6f97c27c025c0dedb9cdb824
Konstantin Kiselyov <kiselev@anyvoid.ru>
2013-06-13 14:53:11 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Runaway::Application.config.secret_token = 'c11f5e2081504abc909cab07ceb1a21012421ddcac2d4b6875c681561bde696c0fc115bb70aa22662b1aac7f106283b1d86c238e18b2af2e5aa2baaca7aef145'
