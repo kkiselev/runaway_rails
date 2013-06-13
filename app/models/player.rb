1b1bfa11603eb2de28c90b79421a2a030a0126b3
Konstantin Kiselyov <kiselev@anyvoid.ru>
2013-06-13 16:25:16 +0400

    Add Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
-class Game < ActiveRecord::Base
-  attr_accessible :area, :name
-  has_many :players
+class Player < ActiveRecord::Base
+  attr_accessible :loc, :name
+  belongs_to :game
 
 	# By default, use the GEOS implementation for spatial columns.
   self.rgeo_factory_generator = RGeo::Geos.factory_generator
