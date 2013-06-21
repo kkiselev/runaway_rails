e6476be55706fe5f0211cc86b7733d73f8cd0936
Konstantin Kiselyov <kiselev@anyvoid.ru>
2013-06-21 13:50:13 +0400

    Ololo

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,16 @@
 class ApiController < ApplicationController
 	include ApiHelper
-	
+
 	def change_location
+		player_id = params[:player_id]
 		api_response_with({:game_id => params[:id]})
 	end
+
+	def locations
+		
+	end
+
+	def take_treasure
+	end
+
 end
