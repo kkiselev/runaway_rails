57e538deebd620d0f7f6ecc63b4e114efb3d90e0
Konstantin Kiselyov <kiselev@anyvoid.ru>
2013-06-29 20:23:21 +0400

    Change back player spatial factory generator

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,7 +4,6 @@ class Player < ActiveRecord::Base
   belongs_to :account
 
   self.rgeo_factory_generator = RGeo::Geos.factory_generator
-	set_rgeo_factory_for_column(:loc, RGeo::Geographic.spherical_factory(:srid => 4326))
 
   class << self
 
